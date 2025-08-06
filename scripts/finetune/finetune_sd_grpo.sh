173d55a217fc93f0dafba63c6879cb89f52a38e3
XueZeyue <44594176+XueZeyue@users.noreply.github.com>
2025-08-06 14:14:20 +0800

    add hpsv3 for sd

--- scripts/finetune/finetune_sd_grpo.sh
+++ scripts/finetune/finetune_sd_grpo.sh
@@ -12,5 +12,13 @@ cd ..
 
 mkdir images_same
 
+
+# install these packages if you want to use hpsv3
+#git clone https://github.com/MizzenAI/HPSv3.git
+#cd HPSv3
+#pip install -e .
+#cd ..
+#pip3 install tf-keras
+
 torchrun --nproc_per_node=8 --master_port 19001 \
 fastvideo/train_grpo_sd.py --config fastvideo/config_sd/dgx.py:hpsv2
