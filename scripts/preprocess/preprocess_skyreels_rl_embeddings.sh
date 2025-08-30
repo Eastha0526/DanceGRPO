061149b6f0427ee6fe6e355cb2a87da2f6dc098c
Zeyue Xue <xuezeyue@connect.hku.hk>
2025-08-30 13:54:36 +0800

    fix a bug for i2v

--- scripts/preprocess/preprocess_skyreels_rl_embeddings.sh
+++ scripts/preprocess/preprocess_skyreels_rl_embeddings.sh
@@ -13,6 +13,10 @@ torchrun --nproc_per_node=$GPU_NUM --master_port 19002 \
     --model_type hunyuan_hf
 
 
+GPU_NUM=8 # 2,4,8
+MODEL_PATH="data/SkyReels-I2V"
+OUTPUT_DIR="data/empty"
+
 torchrun --nproc_per_node=$GPU_NUM --master_port 19003 \
     fastvideo/data_preprocess/preprocess_hunyuan_embeddings.py \
     --model_path $MODEL_PATH \
