eff484dc58a47f98c306a8a5c2a9efb2f7831233
Zeyue Xue <xuezeyue@connect.hku.hk>
2025-09-04 23:54:43 +0800

    fix the path for qwen image edit

--- scripts/preprocess/preprocess_qwen_image_edit_rl_embeddings.sh
+++ scripts/preprocess/preprocess_qwen_image_edit_rl_embeddings.sh
@@ -3,8 +3,9 @@ pip install diffusers==0.35.0 peft==0.17.0 transformers==4.56.0
 
 
 GPU_NUM=8 # 2,4,8
-MODEL_PATH="data/qwenimage"
+MODEL_PATH="data/qwenimage_edit"
 OUTPUT_DIR="data/rl_embeddings"
+
 
 torchrun --nproc_per_node=$GPU_NUM --master_port 19002 \
     fastvideo/data_preprocess/preprocess_qwenimage_edit_embeddings.py \
